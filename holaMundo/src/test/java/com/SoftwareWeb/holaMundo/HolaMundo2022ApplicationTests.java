ace9642972963abeaf58e9ff2db69dfcf33de6fb
Geo69-ops <andres.bohorquez2013@gmail.com>
2022-08-23 11:11:46 -0500

    Carpeta de archivos para pagina web
    aquí.. por medio de IntellJ IDEA se crean las clases

---
+++ holaMundo/src/test/java/com/SoftwareWeb/holaMundo/HolaMundo2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SoftwareWeb.holaMundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaMundo2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
